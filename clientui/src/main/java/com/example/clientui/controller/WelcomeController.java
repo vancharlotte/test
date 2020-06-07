f7752955a3ad552e5fbfcec57fc03532f283d94f
charlotte <vanhuyse.charlotte@gmail.com>
2020-06-06 20:26:53 -0400

    update views : accueil,booksearch (clientui)

--- clientui/src/main/java/com/example/clientui/controller/WelcomeController.java
+++ clientui/src/main/java/com/example/clientui/controller/WelcomeController.java
@@ -1,28 +1,17 @@
 package com.example.clientui.controller;
 
-import com.example.clientui.beans.BookBean;
-import com.example.clientui.beans.CopyBean;
-import com.example.clientui.beans.LoanBean;
-import com.example.clientui.client.LibraryBookClient;
-import com.example.clientui.client.LibraryLoanClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
 
 @Controller
 public class WelcomeController {
 
 
-
     @RequestMapping("/")
     public String accueil() {
-        return "Accueil";
+        return "test";
     }
-
 
 
 }
