0f15b213a5fef7606b1fb8e8cae515acd1bc5818
charlotte <vanhuyse.charlotte@gmail.com>
2020-06-08 15:08:42 -0400

    create view : login (clientui)

--- clientui/src/main/java/com/example/clientui/controller/WelcomeController.java
+++ clientui/src/main/java/com/example/clientui/controller/WelcomeController.java
@@ -1,6 +1,7 @@
 package com.example.clientui.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -8,10 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WelcomeController {
 
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String accueil() {
         return "test";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "Login";
+    }
+
 
 
 }
