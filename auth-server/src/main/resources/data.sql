e1e6884dea281a19c093ebbce78d4310d31001d0
charlotte <vanhuyse.charlotte@gmail.com>
2020-07-09 18:30:02 -0400

    update auth-server bdd

--- auth-server/src/main/resources/data.sql
+++ auth-server/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO oauth_client_details (client_id, client_secret, web_server_redirect_uri, scope, access_token_validity, refresh_token_validity, resource_ids, authorized_grant_types, autoapprove, additional_information)
 VALUES
-('client', '{noop}secret', 'http://localhost:8000/login', 'READ,WRITE', '3600', '10000', 'library-book,library-loan', 'authorization_code,password,refresh_token,implicit', 1, '{}'),
-('batch', '{noop}secret', 'http://localhost:9005/login', 'READ', '3600', '10000', 'library-loan', 'client_credentials', 1, '{}');
+('client', '{noop}secret', 'http://localhost:8000/login', 'READ,WRITE', '3600', '10000', 'library-book,library-loan', 'authorization_code,password,refresh_token,implicit', 'true', '{}'),
+('batch', '{noop}secret', 'http://localhost:9005/login', 'READ', '3600', '10000', 'library-loan', 'client_credentials', 'true', '{}');
 
 
 INSERT INTO Account
