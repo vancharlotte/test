9bf12663d0889ed154ebece07732e6b4080618d8
charlotte <vanhuyse.charlotte@gmail.com>
2020-07-16 14:24:27 -0400

    add eureka to library-book and library-loan

--- auth-server/src/main/java/com/example/authserver/AuthorizationServerApplication.java
+++ auth-server/src/main/java/com/example/authserver/AuthorizationServerApplication.java
@@ -3,10 +3,12 @@ package com.example.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+//@EnableDiscoveryClient
 public class AuthorizationServerApplication {
 
 	public static void main(String[] args) {
--- auth-server/src/main/resources/data.sql
+++ auth-server/src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO oauth_client_details (client_id, client_secret, web_server_redirect_uri, scope, access_token_validity, refresh_token_validity, resource_ids, authorized_grant_types, autoapprove, additional_information)
 VALUES
-('client', '{bcrypt}$12$JCzlOjEfkCiuHRT6sdMyHu8lWLI2H.02qdt75Oe6XVH7NsHMACl0q', 'http://localhost:8000/login', 'READ,WRITE', '3600', '10000', 'library-book,library-loan', 'authorization_code,password,refresh_token,implicit', 'true', '{}'),
-('batch', '{bcrypt}$12$JCzlOjEfkCiuHRT6sdMyHu8lWLI2H.02qdt75Oe6XVH7NsHMACl0q', 'http://localhost:9005/login', 'READ', '3600', '10000', 'library-loan', 'client_credentials', 'true', '{}');
+('client', '{noop}secret', 'http://localhost:8000/login', 'READ,WRITE', '3600', '10000', 'library-book,library-loan', 'authorization_code,password,refresh_token,implicit', 'true', '{}'),
+('batch', '{noop}secret', 'http://localhost:9005/login', 'READ', '3600', '10000', 'library-loan', 'client_credentials', 'true', '{}');
 
 
 INSERT INTO Account
